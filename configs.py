4545f20e3fd8a65a1321d509651f876bc8308a82
Alokchauhan11 <153092293+Alokchauhan11@users.noreply.github.com>
2024-05-28 06:57:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config(object):
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6295345515"))
-	DATABASE_URL = os.environ.get("mongodb://alokchauhan:alokchauhan@ac-4dllcla-shard-00-00.csh0n6l.mongodb.net:27017,ac-4dllcla-shard-00-01.csh0n6l.mongodb.net:27017,ac-4dllcla-shard-00-02.csh0n6l.mongodb.net:27017/?replicaSet=atlas-nd74bj-shard-0&ssl=true&authSource=admin")
+	DATABASE_URL = os.environ.get("mongodb+srv://frankanimes:qayplmwsxokn@cluster0.xksjqdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001874884561")
 	LOG_CHANNEL = os.environ.get("-1002011327804", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
